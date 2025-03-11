8f39730531591ec8e53e8c7cc380b9aadcd19bc1
Ryan Park <rypark@stanford.edu>
2025-03-10 23:03:52 -0700

    Add new cnofigs

---
+++ scripts/baselines/npa_mindsmall_pretrainedemb_celoss_bertsent.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=iris
+#SBATCH --partition=iris-hi
+#SBATCH --nodes=1
+#SBATCH --mem=128G
+#SBATCH --gres=gpu:l40s:1
+#SBATCH --time=240:00:00
+#SBATCH --job-name=train
+#SBATCH --output slurm/%j.out
+
+ulimit -n 64000
+source ~/.bashrc
+conda activate news
+cd /iris/u/rypark/code/newsreclib
+pwd
+
+python newsreclib/train.py experiment=npa_mindsmall_pretrainedemb_celoss_bertsent
